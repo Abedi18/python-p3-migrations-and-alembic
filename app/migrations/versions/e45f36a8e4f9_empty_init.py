13fb2ffe9f6c972cf2c3bb6eb40ca4a560e324e4
Abednego <abedikroos15@gmail.com>
2023-06-12 09:28:05 +0300

    Migrations and Alembic

---
+++ app/migrations/versions/e45f36a8e4f9_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: e45f36a8e4f9
+Revises: 
+Create Date: 2023-06-12 09:23:02.323063
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e45f36a8e4f9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
